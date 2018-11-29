81f94211137cb4befd0602237b206dcede75c8d1
JGE <jochen.gaiser@schmalz.de>
2018-11-29 15:21:52 +0100

    size tabs

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,31 +6,59 @@ $(document).ready(function () {
         show_label: true
     });
 
-    $("#length").slider().on('slideStop', function(ev){
-           var selectedValue = Array.from($(this).data("slider-values"))[ev.value-1];
-           $("#length-selectedValue").val(selectedValue)
-           validate();
-      });
+    $("#length").slider().on('slideStop', function (ev) {
+        var selectedValue = Array.from($(this).data("slider-values"))[ev.value - 1];
+        var selectedSize =  Array.from($(this).data("slider-ticks-labels"))[ev.value - 1];
 
-    $("#width").slider().on('slideStop', function(ev){
-           var selectedValue = Array.from($(this).data("slider-values"))[ev.value-1];
-           $("#width-selectedValue").val(selectedValue)
-           validate();
-      });
+        $("#length-selectedValue").val(selectedValue);
+        $("#length-selectedValue-text").html(selectedValue);
 
-    $("#height").slider().on('slideStop', function(ev){
-           var selectedValue = Array.from($(this).data("slider-values"))[ev.value-1];
-           $("#height-selectedValue").val(selectedValue)
-           validate();
-      });
+        $(".length-images").hide();
+        $(".length-description").hide();
+        $("#length-image-"+selectedSize).show();
+        $("#length-text-"+selectedSize).show();
 
-    function validate(){
+        validate();
+    });
+
+    $("#width").slider().on('slideStop', function (ev) {
+        var selectedValue = Array.from($(this).data("slider-values"))[ev.value - 1];
+        var selectedSize =  Array.from($(this).data("slider-ticks-labels"))[ev.value - 1];
+
+        $("#width-selectedValue").val(selectedValue);
+        $("#width-selectedValue-text").html(selectedValue);
+
+        $(".width-images").hide();
+        $(".width-description").hide();
+        $("#width-image-"+selectedSize).show();
+        $("#width-text-"+selectedSize).show();
+
+        validate();
+    });
+
+    $("#height").slider().on('slideStop', function (ev) {
+        var selectedValue = Array.from($(this).data("slider-values"))[ev.value - 1];
+        var selectedSize =  Array.from($(this).data("slider-ticks-labels"))[ev.value - 1];
+
+        $("#height-selectedValue").val(selectedValue);
+        $("#height-selectedValue-text").html(selectedValue);
+
+        $(".height-images").hide();
+        $(".height-description").hide();
+        $("#height-image-"+selectedSize).show();
+        $("#height-text-"+selectedSize).show();
+
+        validate();
+    });
+
+    function validate() {
         $("#productform").ajaxSubmit({
             url: $('#productform').attr('action'),
             type: 'post',
-            success:  function(price) {
-                   $("#price").html(price);
-                }});
+            success: function (price) {
+                $("#price").html(price);
+            }
+        });
     }
 
 
